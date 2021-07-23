93564c852fc5912a032ada20a619b57c645d4261
Gaurangi <54896258+Coder-Gaurangi@users.noreply.github.com>
2021-07-23 22:47:52 +0530

    Add files via upload

---
+++ Week1/sortedsearch.java
@@ -0,0 +1,57 @@
+/*Gaurangi Tripathi B.Tech CSE Section E 2014657
+  Given an array of sorted integers, design a linear algorithm and implement it using a program to find whether given key element is present in the array or not.
+  Also, find total number of comparisons for each input case.
+  (Time Complexity = O(n), where n is the size of input) 
+  Date of Work: 18-07-21
+  */
+package prg;
+import java.util.Scanner;
+class sortedsearch
+{
+public static void main(String[] args) 
+    {
+    	Scanner sc = new Scanner(System.in);
+		System.out.println("Enter array length: ");
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		System.out.println("Enter elements of array: ");
+		for(int i = 0; i < n; i++)
+		{
+			arr[i] = sc.nextInt();
+		}
+		System.out.println("Enter element to search: ");
+		int x = sc.nextInt();
+        int res = binarySearch(arr,x);
+        if(res == -1)
+			System.out.print("Element is not present in array");
+		else
+			System.out.print("Present " + (res+1));
+		sc.close();
+    }
+ private static int binarySearch(int arr[], int x)
+    {
+    	int i;
+        if(arr == null || arr.length < 0)
+        {
+        	System.out.print("Not Present ");
+        }
+        int end = arr.length-1;
+        for( i = 0; i <=end;)
+        {
+            int mid = i + (end-i)/2;
+            if(x == arr[mid])
+            { 
+            	return mid;
+            }
+            else if(x>arr[mid])
+            {
+                i++;
+            }
+            else if(x<arr[mid])
+            {
+                end--;
+            }
+        }
+        return -1;
+    }
+}
