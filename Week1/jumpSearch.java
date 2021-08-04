0a19a59a93bf9e06a8c2d41b43ca738fb2bfdadf
Gaurangi <54896258+Coder-Gaurangi@users.noreply.github.com>
2021-08-04 11:32:31 +0530

    Update jumpSearch.java

--- Week1/jumpSearch.java
+++ Week1/jumpSearch.java
@@ -1,5 +1,10 @@
 /*Gaurangi Tripathi B.Tech CSE Section E 2014657
- 	Given an already sorted array of positive integers, design an algorithm and implement it using a program to find whether a given key element is present in the sorted array or not. For an array arr[n], search at the indexes arr[0], arr[2], arr[4],.....,arr[2k] and so on. Once the interval (arr[2k]< key < arr[ 2k+1] ) is found, perform a linear search operation from the index 2k to find the element key. (Complexity < O(n), where n is the number of elements need to be scanned for searching):*/
+	Jump Search is a searching algorithm for sorted arrays. Here, we try to search fewer number of elements compared to linear search algorithm which can be done by skipping some fixed number of array elements or jumping ahead by fixed number of steps in every iteration.
+	Approach: We find a block/jump size(m) and then start searching from index 0, then 'm', then '2m', and do on.
+		  Optimal size of 'm' is 'n^(1/2)'.
+		  We find a range where target can lie, and then perform linear search in that range.
+	Complexity: O(n^(1/2))
+*/
 package prg;
 import java.util.Scanner;
 public class jumpSearch
