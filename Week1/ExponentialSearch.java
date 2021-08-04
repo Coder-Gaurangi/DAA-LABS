c8960e81bb6478a327acedb12ca174019d77493a
Gaurangi <54896258+Coder-Gaurangi@users.noreply.github.com>
2021-08-04 11:22:13 +0530

    Update ExponentialSearch.java

--- Week1/ExponentialSearch.java
+++ Week1/ExponentialSearch.java
@@ -1,5 +1,5 @@
 /*Gaurangi Tripathi B.Tech CSE Section E 2014657
- 	Given an already sorted array of positive integers, design an algorithm and implement it using a program to find whether a given key element is present in the sorted array or not. For an array arr[n], search at the indexes arr[0], arr[2], arr[4],.....,arr[2k] and so on. Once the interval (arr[2k]< key < arr[ 2k+1] ) is found, perform a linear search operation from the index 2k to find the element key. (Complexity < O(n), where n is the number of elements need to be scanned for searching):
+	Exponential Search is an algorithm used for searching sorted arrays. We find the range where the target value resides and then perform binary/linear search in the range.
  	Approach: We are starting with 0th element, if element is found, then we return 0.
  			  Else we go to 1st index ( i = 1), check if ith index is less than length of array and element at that index is less than or equal to key element then we double the index.
  			  If the above condition fails i.e. either index position has exceeded array length or the element at an index is greater than the key means that we have found the range where key lies.
