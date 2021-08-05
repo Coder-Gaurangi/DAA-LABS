cd7b837aadfa4a9aa1f2561129d4046bc101ccee
Gaurangi <54896258+Coder-Gaurangi@users.noreply.github.com>
2021-08-05 13:55:22 +0530

    Add files via upload

---
+++ Week2/Problem3.java
@@ -0,0 +1,43 @@
+/* Gaurangi Tripathi B. Tech CSE Section E 2014657
+ Given an array of nonnegative integers, design an algorithm and a program to count the number
+of pairs of integers such that their difference is equal to a given key, K. */
+
+package prg;
+import java.util.Scanner;
+public class Problem3 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("\nEnter number of test cases: ");
+	    int t = sc.nextInt();
+	    while(t-- > 0)
+		{
+	    	System.out.println("Enter array length: ");
+	    	int n = sc.nextInt();
+	    	int arr[] = new int[n];
+	    	System.out.println("Enter array elements: ");
+	    	for(int i = 0; i < n; i++)
+	    	{
+	    		arr[i] = sc.nextInt();
+	    	}
+	    	System.out.println("Enter key: ");
+	    	int key = sc.nextInt();
+	    	System.out.println("Number of pairs with difference " + key + "are: " + count(arr,n,key));
+	    }
+	    sc.close();
+	}
+	static int count(int arr[], int n, int key)
+	{
+		int c = 0;
+		for(int i = 0; i < n; i++)
+		{
+			for (int j = i + 1; j < n; j++)
+                if (arr[i] - arr[j] == key || arr[j] - arr[i] == key)
+                    c++;
+        }
+        return c;
+	}
+}
+
+
