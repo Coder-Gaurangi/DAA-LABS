cf46de4c698b471df0112980384ba3932f38a251
Gaurangi <54896258+Coder-Gaurangi@users.noreply.github.com>
2021-08-05 10:50:35 +0530

    Update sortedsearch.java

--- Week1/sortedsearch.java
+++ Week1/sortedsearch.java
@@ -11,7 +11,7 @@ class sortedsearch
 public static void main(String[] args) 
     {
     	Scanner sc = new Scanner(System.in);
-		System.out.println("\nEnter number of test cases: ");
+	    System.out.println("\nEnter number of test cases: ");
         int t = sc.nextInt();
         while(t-- > 0)
 	{
@@ -32,7 +32,7 @@ public static void main(String[] args)
 			System.out.print("Present " + (res+1));
 	}
 	sc.close();
-    }
+   }
  private static int binarySearch(int arr[], int x)
     {
     	int i;
