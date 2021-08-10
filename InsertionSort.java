90c8b76c58e85557da57ee37c26cc55c889dd395
Gaurangi <54896258+Coder-Gaurangi@users.noreply.github.com>
2021-08-10 13:47:56 +0530

    Add files via upload

---
+++ InsertionSort.java
@@ -0,0 +1,46 @@
+package prg;
+import java.util.Scanner;
+class InsertionSort {
+	void sort(int arr[])
+	{
+		int n = arr.length;
+		for (int i = 1; i < n; ++i) {
+			int key = arr[i];
+			int j = i - 1;
+			while (j >= 0 && arr[j] > key) {
+				arr[j + 1] = arr[j];
+				j = j - 1;
+			}
+			arr[j + 1] = key;
+		}
+	}
+	static void printArray(int arr[])
+	{
+		int n = arr.length;
+		for (int i = 0; i < n; ++i)
+			System.out.print(arr[i] + " ");
+
+		System.out.println();
+	}
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter no of test cases: ");
+		int t = sc.nextInt();
+		while(t-- > 0)
+		{
+		System.out.println("Enter array length: ");
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		System.out.println("Enter array elements: ");
+		for(int i = 0; i < n; i++)
+		{
+			arr[i] = sc.nextInt();
+		}
+		InsertionSort ob = new InsertionSort();
+		ob.sort(arr);
+		printArray(arr);
+		}
+		sc.close();
+	}
+}
