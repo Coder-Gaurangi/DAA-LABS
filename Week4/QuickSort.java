fa810384edd4e2fead1083feb6e4352055b41d63
Gaurangi <54896258+Coder-Gaurangi@users.noreply.github.com>
2021-09-27 15:35:36 +0530

    Update QuickSort.java

--- Week4/QuickSort.java
+++ Week4/QuickSort.java
@@ -1,5 +1,5 @@
 /* Gaurangi Tripathi B.Tech CSE Section E 2014657
- 2
+ 
    Given an unsorted array of integers, design an algorithm and implement it using a program to sort an array of elements by partitioning the array into two subarrays
    based on a pivot element such that one of the sub array holds values smaller than the pivot element while another sub array holds values greater
    than the pivot element Pivot element should be selected randomly from the array. 
